fe26f8b0a968e399527a7efabeb4195d611a27fd
Victor <vr011121@gmail.com>
2023-11-28 16:33:19 -0500

    Added config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const DB_PARAMS = {
+    host: "127.0.0.1",
+    user: "user",
+    password: "password",
+    database: "database",
+    multipleStatements: true
+};
+
+module.exports = {
+    DB_PARAMS : DB_PARAMS
+};
